3737d4ae20ea619b19671a58104778898e93d4d9
cwillison94 <cwillison94@gmail.com>
2016-11-08 19:20:39 -0500

    Removed some files from git

---
+++ EasyATM/ContinuePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using EasyATM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyATM
+{
+    /// <summary>
+    /// Interaction logic for ContinuePage.xaml
+    /// </summary>
+    public partial class ContinuePage : Page
+    {
+        private Client client = null;
+
+        public ContinuePage(Client client)
+        {
+            InitializeComponent();
+            this.client = client;
+        }
+
+        private void ButtonComplete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAnotherTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new OptionsPage(this.client));
+        }
+    }
+}
